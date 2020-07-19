72e63ebdc4b3145f8386698d540dd65a9d72cb95
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2020-07-19 12:18:22 +0000

    Initial Commit

---
+++ D3 DNS/D3_DNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  D3_DNSApp.swift
+//  D3 DNS
+//
+//  Created by Jay on 19/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct D3_DNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
